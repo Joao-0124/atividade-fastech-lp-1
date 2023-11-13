0553a8835a22ff4dbb17ad504c2d0d800dd42121
unknown <joao.miranda20018@gmail.com>
2023-11-12 23:40:08 -0300

    Atividade 1 Finalizada

--- 1-atividade.py
+++ 1-atividade.py
@@ -1,16 +1,21 @@
 cont = 0
+
 contO = 0
 contB = 0
 contR = 0
 contP = 0
+
 idade_maior = 0
 idade_menor = 1000
 idade_menor_resp = ''
 idade_maior_resp = ''
+
 while True:
     idade = int(input("Qual Sua Idade? "))
     atendimento = input("Seu Atendimento foi (OTIMO, BOM, REGULAR, PESSIMO)? ")
+
     cont += 1
+
     if (atendimento == "OTIMO"):
         contO += 1
     if (atendimento == "BOM"):
@@ -19,14 +24,17 @@ while True:
         contR += 1
     if (atendimento == "PESSIMO"):
         contP += 1
+
     if (idade > idade_maior):
         idade_maior = idade
         idade_maior_resp = atendimento
     if (idade < idade_menor):
         idade_menor = idade
         idade_menor_resp = atendimento
+
     if (input("Deseja Continuar? (S, N)" )) != "S":
         break
+    
 print("Total de pessoas que responderam", cont)
 print("A quantidade de pessoas que receberam otimo foi:\n" , contO)
 print("A quantidade de pessoas que receberam bom foi:\n" , contB)
