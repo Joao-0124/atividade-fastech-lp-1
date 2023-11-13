dd4fb5138022cdcca99b0c2b35542ed080533b25
unknown <joao.miranda20018@gmail.com>
2023-11-12 23:33:40 -0300

    Atividade 2 finalizada

--- 2-atividade.py
+++ 2-atividade.py
@@ -1,59 +1,47 @@
-#Elevadores A, B, C
-#Períodos Matutino, Vespertino, Noturno
-#Elevador mais utilizado e seu período com mais fluxo
-#Qual Período mais utilizado
-#Qual Período menos utilizado
-cont = 0
 contA = 0
 contB = 0
 contC = 0
+
 contM = 0
 contV = 0
 contN = 0
+
+lista = []
+lista_elevador = []
+lista_periodo = []
+
 while True:
     elevador = input("Qual o Elevador que você Utilizou: (A, B ou C)" )
     periodo = input("Qual o Período Utilizado \n Para Matutino Informe M \n Para Vespertino informe V \n Para Noturno informe N \n " )
-    cont += 1
+    
     if(elevador == "A"):
         contA += 1
-    if(elevador == "B"):
+    elif(elevador == "B"):
         contB += 1
-    if(elevador == "C"):
+    elif(elevador == "C"):
         contC += 1
+
     if(periodo == "M"):
         contM += 1
-    if(periodo == "V"):
+    elif(periodo == "V"):
         contV += 1
-    if(periodo == "N"):
+    elif(periodo == "N"):
         contN += 1
     if(input("Deseja Continuar? (S, N)" )) != "S":
         break
 
+lista_elevador.extend([contA,contB,contC])
+lista_periodo.extend([contM,contV,contN])
 
-lista_elevador = [contA, contB, contC]
-elevador_mais_utilizado = max(lista_elevador)
-if elevador_mais_utilizado == contA:
-    elevador_mais_utilizado = 'A'
-elif elevador_mais_utilizado == contB:
-    elevador_mais_utilizado = 'B'
-else:
-    elevador_mais_utilizado = 'C'
+elevadores = {'A': contA, 'B': contB, 'C': contC}
+periodos = {'M': contM, 'V': contV, 'N': contN}
 
-lista_periodo = [contM, contV, contN]
-periodo_de_maior_fluxo = max(lista_periodo)
-if(periodo_de_maior_fluxo == contM):
-    contM = 'Matutino'
-elif(periodo_de_maior_fluxo == contV):
-    contV = 'Vespertino'
-else:
-    contN = 'Noturno'
-periodo_de_menor_fluxo = min(lista_periodo)
-if(periodo_de_menor_fluxo == contM):
-    contM = 'Matutino'
-elif(periodo_de_menor_fluxo == contV):
-    contV = 'Vespertino'
-else:
-    contN = 'Noturno'
-print(f"O Elevador mais Utilizado foi: {elevador_mais_utilizado} no Período {periodo}")
-print("O Período em que os Elevadores foram mais Utilizados foi:" , periodo_de_maior_fluxo)
-print("O Período em que os Elevadores foram mais Utilizados foi:" , periodo_de_menor_fluxo)
+max_lista_elevador = max(elevadores, key=elevadores.get)
+max_lista_periodo = max(periodos, key=periodos.get)
+min_lista_periodo = min(periodos, key=periodos.get)
+
+lista.append([max_lista_elevador, max_lista_periodo])
+
+print(f"O Elevador mais Utilizado foi o {max_lista_elevador} e o Período com mais fluxo foi o {max_lista_periodo}")
+print("O Período em que os Elevadores foram mais Utilizados foi:" , max_lista_periodo)
+print("O Período em que os Elevadores foram menos Utilizados foi:" , min_lista_periodo)
